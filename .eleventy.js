abec70809669c58ba89e4091d7834f30485824da
Steven <info@stevenpark.co.uk>
2020-03-17 11:38:01 +0000

    Add base template (and alias)

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,23 @@
+const CleanCSS = require('clean-css');
+const Terser = require("terser");
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+
+  eleventyConfig.addFilter('cssmin', function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addFilter("jsmin", function(code) {
+      let minified = Terser.minify(code);
+      if( minified.error ) {
+          console.log("Terser error: ", minified.error);
+          return code;
+      }
+      return minified.code;
+  });
 
   eleventyConfig.addPassthroughCopy("assets/img");
   eleventyConfig.addPassthroughCopy("assets/js");
-  eleventyConfig.addPassthroughCopy("assets/css");
   eleventyConfig.addPassthroughCopy("assets/css/fonts");
 
 };
