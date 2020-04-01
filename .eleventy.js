d4b26af59fed150abfca630e0cc710183d073c08
Steven <info@stevenpark.co.uk>
2020-04-01 12:16:01 +0100

    Addition to #fb96a6c - Process CSS differently (not including SASS); tweak metadata content;

--- .eleventy.js
+++ .eleventy.js
@@ -42,6 +42,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("assets/img");
   eleventyConfig.addPassthroughCopy("assets/js");
+  eleventyConfig.addPassthroughCopy("assets/css");
   eleventyConfig.addPassthroughCopy("assets/css/fonts");
 
 };
