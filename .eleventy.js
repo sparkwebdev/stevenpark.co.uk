cb6bf13e16c74739250b67c2cf02c2a00874e687
Steven Park <info@stevenpark.co.uk>
2020-07-24 11:45:53 +0100

    Favicon and build tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -152,10 +152,20 @@ module.exports = function(eleventyConfig) {
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
 
+  // Assets
   eleventyConfig.addPassthroughCopy("./src/assets/img");
   eleventyConfig.addPassthroughCopy("./src/assets/js");
   eleventyConfig.addPassthroughCopy("./src/assets/css");
   eleventyConfig.addPassthroughCopy("./src/assets/css/fonts");
+
+  // Favicon, PWA, robots, etc
+  eleventyConfig.addPassthroughCopy('./src/robots.txt');
+  eleventyConfig.addPassthroughCopy('./src/browserconfig.xml');
+  eleventyConfig.addPassthroughCopy('./src/site.webmanifest');
+  eleventyConfig.addPassthroughCopy("./src/*.png");
+  eleventyConfig.addPassthroughCopy("./src/*.ico");
+  eleventyConfig.addPassthroughCopy("./src/safari-pinned-tab.svg");
+  
   
 
   return {
