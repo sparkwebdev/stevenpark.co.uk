3a9499166a04846b23e200cc60dd5dfb49b253ed
Steven Park <info@stevenpark.co.uk>
2025-07-21 10:19:25 +0100

    Removed dated testimonial reference; fix spacing issue on contact form

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -14,7 +14,7 @@ if (contactForm) {
     })
     .then(res => {
       if (res) {
-        contactForm.parentNode.innerHTML = '<div role="alert"><h2>Message sent</h2><p><strong>Thank you for your enquiry. I will get back to you ASAP.</strong></p></div>';
+        contactForm.parentNode.innerHTML = '<div class="flow" role="alert"><h2>Message sent</h2><p><strong>Thank you for your enquiry. I will get back to you ASAP.</strong></p></div>';
         // if (navigator.vibrate) {
         //   navigator.vibrate(200);
         // }
